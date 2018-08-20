6882458119fc0afd5ba5dc5b814141058403e5b5
matt <matthew.dobrowolski5@gmail.com>
2018-08-20 16:35:42 -0600

    fix???

--- app/components/todo/todo-service.js
+++ app/components/todo/todo-service.js
@@ -13,7 +13,6 @@ function logError(e) {
 let todoList = []
 
 export default class TodoService {
-<<<<<<< HEAD
 
 	getTodos(draw) {
 		console.log("Getting the Todo List")
@@ -60,57 +59,4 @@ export default class TodoService {
 
 	}
 
-=======
-  getTodos(draw) {
-	console.log("Getting the Todo List");
-	
-    todoApi
-      .get("")
-      .then(res => {
-        // <-- WHY IS THIS IMPORTANT????
-        todoList = res.data.data;
-        let k = todoList;
-        console.log(k)
-        draw(k);
-      })
-      .catch(logError);
-  }
-
-  addTodo(todo, call) {
-    // WHAT IS THIS FOR???
-    todoApi
-      .post("", todo)
-      .then(function(res) {
-        // <-- WHAT DO YOU DO AFTER CREATING A NEW TODO?
-
-        todoList.push(todo);
-        call(res);
-      })
-      .catch(logError);
-  }
-
-  toggleTodoStatus(todoId) {
-    // MAKE SURE WE THINK THIS ONE THROUGH
-    //STEP 1: Find the todo by its index **HINT** todoList
-	const todo = todoList.find(todo => todo._id == todoId);
-console.log(todo)
-	
-    todo.completed = !todo.completed; ///MODIFY THIS LINE
-
-    //STEP 2: Change the completed flag to the opposite of what is is **HINT** todo.completed = !todo.completed
-    todoApi
-      .put(todoId, todo)
-      .then(function(res) {
-        call(res); //DO YOU WANT TO DO ANYTHING WITH THIS?
-      })
-      .catch(logError);
-  }
-
-  removeTodo(todoId) {
-    todoApi.delete(todoId);
-    this.getTodos(call);
-
-    // Umm this one is on you to write.... The method is a DELETE
-  }
->>>>>>> 8f7e7214be913be16c24a991764432b0e74a58a2
 }
